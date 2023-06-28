034a68ed91c337dae2993c2689a688fc129b2969
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-28 19:50:39 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+#REMOVEME? YNH_PHP_VERSION="7.4"
 
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-fpm librsvg2-bin pdftk imagemagick potrace"
+#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-fpm librsvg2-bin pdftk imagemagick potrace"
 
 #=================================================
 # PERSONAL HELPERS
